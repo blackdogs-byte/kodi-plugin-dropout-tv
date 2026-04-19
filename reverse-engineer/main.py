c29472212bfa5a7a8a0074f245330ccaf2b73748
h3rby7 <herbielutz@gmx.net>
2026-04-19 14:39:35 +0200

    RE move login to function, add logout function

--- reverse-engineer/main.py
+++ reverse-engineer/main.py
@@ -60,6 +60,8 @@ import jwt
 host = f'watch.dropout.tv'
 baseUrl = f'https://{host}/'
 loginUrl = f'{baseUrl}login'
+logoutUrl = f'{baseUrl}logout'
+aVideoUrl = f'{baseUrl}parlor-room/season:1/videos/wavelength'
 
 header_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
 header_accept_language = 'en-US,en;q=0.9'
@@ -86,13 +88,23 @@ def to_login_form_data(email: str, password: str, authenticity_token: str, utf8:
   logger.debug(f'LOGIN data is: "{formdata}"')
   return formdata
 
-if __name__ == "__main__":
-  logger.warning("Hardcoded playalong to log into dropout TV")
+def logout(session: requests.Session, csrfToken: str):
+  logger.info('LOGGING OUT')
+  formdata = f'authenticity_token={urllib.parse.quote_plus(csrfToken)}'
+  headers = {
+    'Host': host,
+    'User-Agent': header_user_agent,
+    'Accept-Language': header_accept_language,
+    'Upgrade-Insecure-Requests': '1',
+    'Referer': baseUrl,
+    "Content-Type": 'application/x-www-form-urlencoded',
+    "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  }
+  r = session.post(logoutUrl, headers=headers, data=formdata)
+  logger.debug(f'POST {logoutUrl}\nREDIRECTED to {r.url} and\nRETURNED\n\tHEADERS: {r.headers}')
 
-  ########### Integrated Auth flow? ###########
-  session = requests.Session()
-
-  # VISIT SIGN-IN PAGE
+def login(session: requests.Session, email, password) -> Tuple[Optional[str], str]:
+   # VISIT SIGN-IN PAGE
   headers = {
     'Host': host,
     'User-Agent': header_user_agent,
@@ -101,7 +113,8 @@ if __name__ == "__main__":
     'Referer': baseUrl,
   }
   r = session.get(loginUrl, headers=headers)
-  logger.debug(f'GET {loginUrl}\nRETURNED\n\tHEADERS: {r.headers}\n\tBODY: {r.text}')
+  # logger.debug(f'GET {loginUrl}\nRETURNED\n\tHEADERS: {r.headers}\n\tBODY: {r.text}')
+  logger.debug(f'GET {loginUrl}\nRETURNED\n\tHEADERS: {r.headers}')
 
   logger.debug("Extracting authenticity_token and utf hiden input values...")
   soup = BeautifulSoup(r.text, "html.parser")
@@ -110,7 +123,7 @@ if __name__ == "__main__":
   utf8 = soup.select_one("input[name='utf8']")["value"]
 
   # LOGIN FORM
-  formdata = to_login_form_data(args.email, args.password, authenticity_token, utf8)
+  formdata = to_login_form_data(email, password, authenticity_token, utf8)
 
   headers = {
     'Host': host,
@@ -124,13 +137,71 @@ if __name__ == "__main__":
   }
   r = session.post(loginUrl, headers=headers, data=formdata)
   # Should redirect to /browse
-  logger.debug(f'POST {loginUrl}\nREDIRECTED to {r.url} and\nRETURNED\n\tHEADERS: {r.headers}\nBODY:\n {r.text}')
+  # logger.debug(f'POST {loginUrl}\nREDIRECTED to {r.url} and\nRETURNED\n\tHEADERS: {r.headers}\nBODY:\n {r.text}')
+  logger.debug(f'POST {loginUrl}\nREDIRECTED to {r.url} and\nRETURNED\n\tHEADERS: {r.headers}')
   # that page then contains window.TOKEN as expected for 'after login'...
   window_token = get_window_token(r.text)
-  if not window_token:
-    exit(1)
-  logging.debug(f"Decoding token...")
-  res = jwt.decode(window_token, options={"verify_signature": False, "verify_exp": True})
-  logging.info(f"Token content: {res}")
+  csrf_token: str = soup.select_one("meta[name='csrf-token']")["content"]
+  if window_token:
+    logging.debug(f"Decoding token...")
+    res = jwt.decode(window_token, options={"verify_signature": False, "verify_exp": True})
+    logging.info(f"Token content: {res}")
+  return window_token, csrf_token
 
-  # TODO: what token(s) are used to play a video
+def get_video_iframe_from_text(text: str) -> None | str:
+  """
+  Search for the iframe with title='Video Player'
+  """
+  match = re.search(r'"(https:\/\/embed\.vhx\.tv\/videos\/\S+)"', text)
+  url = match.group(1) if match else None
+  if not isinstance(url, str):
+    logger.error("Video HTML does not have embed.vhx.tv video url")
+    return None
+
+  logger.info(f"Extracted embed.vhx.tv url '{url}'")
+  return url
+
+if __name__ == "__main__":
+  logger.warning("Hardcoded playalong to log into dropout TV and retreive video stream")
+  session = requests.Session()
+  csrfToken: Optional[str] = None
+
+  try:
+    ########### Auth flow ###########
+    windowToken, csrfToken = login(session, args.email, args.password)
+
+    ########### Play Video flow ###########
+    # TODO: Continue here with one video to get the hls streams to vod-adaptive-ak.vimeocdn.com links
+
+    # VISIT SPECIFIC VIDEO PAGE
+    headers = {
+      'Host': host,
+      'User-Agent': header_user_agent,
+      'Accept-Language': header_accept_language,
+      'Upgrade-Insecure-Requests': '1',
+      'Referer': baseUrl,
+    }
+    r = session.get(aVideoUrl, headers=headers)
+    logger.debug(f'GET {loginUrl}\nRETURNED\n\tHEADERS: {r.headers}\n\tBODY: {r.text}')
+    # logger.debug(f'GET {loginUrl}\nRETURNED\n\tHEADERS: {r.headers}')
+
+    iframeUrl = get_video_iframe_from_text(r.text)
+    if not iframeUrl:
+      exit(1)
+
+    # CALL IFRAME
+    headers = {
+      'User-Agent': header_user_agent,
+      'Accept': 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language': header_accept_language,
+      'Upgrade-Insecure-Requests': '1',
+      'Referer': baseUrl,
+      # 'Sec-Fetch-Dest': 'iframe',
+      # 'Sec-Fetch-Mode': 'navigate',
+      # 'Sec-Fetch-Site': 'cross-site',
+    }
+    r = session.get(iframeUrl, headers=headers)
+    logger.debug(f'GET {iframeUrl}\nRETURNED\n\tHEADERS: {r.headers}\n\tBODY: {r.text}')
+  finally:
+    if csrfToken:
+      logout(session, csrfToken)
